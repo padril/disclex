f5b0dc6ce1f9d7245e315847bc4a5539dac78e95
leo peckham <padrillium@gmail.com>
2026-04-27 10:55:51 -0400

    Added recording to paramsearch

--- paramsearch.py
+++ paramsearch.py
@@ -4,12 +4,14 @@ from analyze import *
 
 
 #         alpha    ug_mix
-bounds = [(0.01, 10), (0.0, 2.0)]
+bounds = [(0.01, 10.0), (0.01, 2.0)]
 
 count = 0
+file = open('paramsearch.out', 'w')
+print(f'p\tr\tf\talpha\tug_mix', file=file)
 
 def small_call(x):
-    global count
+    global count, file
     print(f'calls so far: {count}')
     count += 1
 
@@ -17,13 +19,13 @@ def small_call(x):
     ref = parse_lexicon(gold_lexicon)
 
     alpha, ug_mix = x
-    surfaces = "dev10.txt"
+    surfaces = "easy100.txt"
     subprocess.run(["./build/bin/disclex",
                     "--prior", "fst",
                     "--phones", "phones.txt",
                     "--observations", surfaces, 
                     "--phonemes", "phones.txt",
-                    "--steps", str(20),
+                    "--steps", str(100),
                     "--rebuild-stride", str(10),
                     "--alpha", str(alpha),
                     "--universal-grammar-weight", str(ug_mix)], capture_output=True)
@@ -36,7 +38,8 @@ def small_call(x):
     counts = get_counts(forms, deltas)
     hyp = make_lexicon(surfaces, counts, 'gibbs_lexicon.tsv')
     p, r, f = grouping(ref, hyp)
+    print(f'{p}\t{r}\t{f}\t{alpha}\t{ug_mix}', file=file)
 
     return f
 
-print(dual_annealing(small_call, bounds, maxiter=100))
+print(dual_annealing(small_call, bounds))
